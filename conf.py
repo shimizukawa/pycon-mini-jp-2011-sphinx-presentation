7f697dab8f596b69d5cde6117893d2fcf36191d0
shimizukawa <shimizukawa@gmail.com>
2013-06-02 11:28:24 +0900

    initial

---
+++ conf.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+html_use_modindex = False
+html_use_index = False
+extensions = ['sphinxcontrib.themes.s6']
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = u'Sphinxからプレゼンテーションスライドを作ってみた'
+copyright = u'清水川Web'
+html_theme = 's6'
+s6_theme = 'slidejs'
